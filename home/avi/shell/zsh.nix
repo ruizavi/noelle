db73021abb0c4453a4ae7166316b9a6b0b226670
avi <avi.ruiz@proton.me>
2024-01-05 12:40:29 -0600

    minor changes

--- home/avi/shell/zsh.nix
+++ home/avi/shell/zsh.nix
@@ -105,6 +105,10 @@
         --pointer ''
         --marker ''
       "
+
+      export PYENV_ROOT="$HOME/.pyenv"
+      [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+      eval "$(pyenv init -)"
     '';
 
     initExtra = ''
